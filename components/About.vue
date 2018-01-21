3b866dcb901e5f132d4624d1e442c552df24bef3
Samantha Ming <samanthaming.ming@gmail.com>
2018-01-21 13:46:07 -0800

    setup about section

---
+++ components/About.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="about">
+    <el-row justify="center">
+      <el-col :span="24" :md="{span: 14, offset: 5}">
+        <div class="grid-content"></div>
+        <h1>Hi, I'm Samantha, a Front End Web Developer based in Vancouver</h1>
+      </el-col>
+    </el-row>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+  h1 {
+    font-size: 56px;
+    text-align: center;
+  }
+</style>
