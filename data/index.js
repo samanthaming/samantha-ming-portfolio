e27293230bbfe883bdc30f7104c5278a674cf1b0
Samantha Ming <samanthaming.ming@gmail.com>
2018-01-26 17:16:20 -0800

    update json data

--- data/index.js
+++ data/index.js
@@ -1,9 +1,11 @@
 import ChatChing from "~/data/Chat-Ching.json";
 import EventsPodcast from "~/data/Events-Podcast.json";
 import Calendar from "~/data/Calendar.json";
+import MySite from "~/data/My-Site.json";
 
 export default [
   ChatChing,
   EventsPodcast,
   Calendar,
+  MySite,
 ]
