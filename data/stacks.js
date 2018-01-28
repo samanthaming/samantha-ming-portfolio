acb631deb01fbb1b14fb4244f43b33c61aced302
Samantha Ming <samanthaming.ming@gmail.com>
2018-01-27 22:52:21 -0800

    add technologies data

---
+++ data/stacks.js
@@ -0,0 +1,10 @@
+import ProfessionalExperience from "~/data/Professional-Experience.json";
+import Experience from "~/data/Experience.json";
+import Education from "~/data/Education.json";
+
+export default [
+  ProfessionalExperience,
+  Experience,
+  Education
+]
+
